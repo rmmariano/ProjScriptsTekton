f7e94b20fb41f506f4f0bdbc37dd65a7b418919e
Rodrigo Mariano <rodmariano13@gmail.com>
2015-03-24 21:24:38 -0300

    redirecionamento do quero.html corrigido parte 3

--- backend/appengine/routes/quero/home.py
+++ backend/appengine/routes/quero/home.py
@@ -7,7 +7,7 @@ from gaepermission.decorator import login_required
 
 __author__ = 'Rodrigo'
 
-#default é @login_required
+#default é @login_required, porém só ADMIN pode acessar, com o @login_required explicito, qualquer um logado pode acessar
 @login_required
 @no_csrf
 def index():
