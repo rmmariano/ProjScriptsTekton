603b611998f804746ebaa665afea7065f2ecd019
Rodrigo Mariano <rodmariano13@gmail.com>
2015-03-24 21:08:10 -0300

    redirecionamento do quero.html corrigido parte 2

--- backend/appengine/routes/quero/home.py
+++ backend/appengine/routes/quero/home.py
@@ -3,10 +3,12 @@
 from __future__ import absolute_import, unicode_literals
 from gaecookie.decorator import no_csrf
 from config.template_middleware import TemplateResponse
+from gaepermission.decorator import login_required
 
 __author__ = 'Rodrigo'
 
 #default é @login_required
+@login_required
 @no_csrf
 def index():
     return TemplateResponse()
