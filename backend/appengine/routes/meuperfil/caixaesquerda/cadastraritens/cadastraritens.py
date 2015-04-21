814a76f2c644ede0054aa3d669c7ad241ba58e59
Rodrigo Mariano <rodmariano13@gmail.com>
2015-04-21 13:55:17 -0300

    Arrumado estrutura de diretorios em Meus Itens para evitar dor de cabeca quando for fazer os CRUDs

--- backend/appengine/routes/meuperfil/caixaesquerda/cadastraritens/cadastraritens.py
+++ backend/appengine/routes/meuperfil/caixaesquerda/cadastraritens/cadastraritens.py
@@ -9,5 +9,5 @@ __author__ = 'Rodrigo'
 
 @login_required
 @no_csrf
-def index():
-    return TemplateResponse(template_path='/meuperfil/caixaesquerda/itens/itensmeusitens.html')
+def index(): #meuperfil/caixaesquerda/cadastraritens/cadastraritens.html
+    return TemplateResponse(template_path='/meuperfil/caixaesquerda/cadastraritens/cadastraritens.html')
