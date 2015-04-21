267206b6dc9182dd64b24b47e63581c46f98db64
Rodrigo Mariano <rodmariano13@gmail.com>
2015-04-21 18:35:59 -0300

    refs#1 atualizado lista de categorias (dicionario no servidor)

--- backend/appengine/routes/meuperfil/caixaesquerda/cadastraritens/cadastraritens.py
+++ backend/appengine/routes/meuperfil/caixaesquerda/cadastraritens/cadastraritens.py
@@ -4,10 +4,24 @@ from __future__ import absolute_import, unicode_literals
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_required
+from operator import itemgetter
 
 __author__ = 'Rodrigo'
 
+categorias = {'op-lvr-mng-hq':'Livros, Mangás e HQs','op-cd-dvd-bry':'CDs, DVDs e BLU-RAYs','op-vstr':'Vestuário','op-inf':'Informática',
+             'op-brq-gms':'Brinquedos e Games','op-mvs':'Móveis','op-outros':'Outros'}
+
+categorias = dict(sorted(categorias.items(), key=itemgetter(1))) # itemgetter(1) para ordenar pelo valor, se colocar 0, ordena pela chave
+
 @login_required
 @no_csrf
-def index(): #meuperfil/caixaesquerda/cadastraritens/cadastraritens.html
-    return TemplateResponse(template_path='/meuperfil/caixaesquerda/cadastraritens/cadastraritens.html')
+def index():
+    contexto = {'categorias':categorias}
+    return TemplateResponse(contexto,template_path='/meuperfil/caixaesquerda/cadastraritens/cadastraritens.html')
+
+@login_required
+@no_csrf
+def salvar(**kwargs):
+
+    return ''
+
