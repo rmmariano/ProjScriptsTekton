4a0044fb34cb85e1dd47e85c829c99fa8a423a7c
Rodrigo Mariano <rodmariano13@gmail.com>
2015-04-25 14:43:08 -0300

    refs#2

--- backend/appengine/routes/meuperfil/caixaesquerda/cadastraritens/cadastraritens.py
+++ backend/appengine/routes/meuperfil/caixaesquerda/cadastraritens/cadastraritens.py
@@ -3,12 +3,11 @@
 from __future__ import absolute_import, unicode_literals
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
+from gaeforms.ndb.form import ModelForm
 from gaepermission.decorator import login_required
 from operator import itemgetter
 from tekton import router
-#from backend.appengine.models.db import Item
 from google.appengine.ext import ndb
-from gaegraph.model import Node
 from tekton.gae.middleware.redirect import RedirectResponse
 
 __author__ = 'Rodrigo'
@@ -22,19 +21,33 @@ categorias = dict(sorted(categorias.items(), key=itemgetter(1))) # itemgetter(1)
 @login_required
 @no_csrf
 def index():
-    contexto = {'categorias':categorias,'salvar':router.to_path(salvar)}
+    contexto = {'categorias':categorias,'salvar':router.to_path(salvar),
+                'errors':'', 'item':''}
     return TemplateResponse(contexto,template_path='/meuperfil/caixaesquerda/cadastraritens/cadastraritens.html')
 
 @login_required
 @no_csrf
 def salvar(**itens):
-    novo_item = Item(titulo = itens['titulo'], #categoria = itens['select'],
-                     imagem = itens['imagem'], descricao = itens['descricao'])
-    novo_item.put() #eh aqui que salva no DB
+    item_form = ItemForm(**itens)
+
+    errors = item_form.validate()
+
+    if errors:
+        contexto = {'categorias':categorias,'salvar':router.to_path(salvar),
+                    'errors':errors, 'item':item_form}
+        return TemplateResponse(contexto,template_path='/meuperfil/caixaesquerda/cadastraritens/cadastraritens.html')
+    else:
+        novo_item = item_form.fill_model()
+        novo_item.put()
+
     return RedirectResponse('/meuperfil/caixaesquerda/cadastraritens/cadastraritens')
 
-class Item(Node):
+class Item(ndb.Model):
     titulo = ndb.StringProperty(required=True)
     #categoria = ndb.StringProperty(required=True)
     imagem = ndb.StringProperty(required=True)
     descricao = ndb.StringProperty(required=True)
+
+class ItemForm(ModelForm):
+    _model_class_ = Item
+    _include = [Item.titulo, Item.imagem, Item.descricao]
