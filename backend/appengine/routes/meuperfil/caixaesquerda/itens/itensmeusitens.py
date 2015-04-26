67c2327d8801a35428a70ba7a7b6a57bc93cbb92
Rodrigo Mariano <rodmariano13@gmail.com>
2015-04-26 16:37:35 -0300

    refs#2 D do CRUD feito

--- backend/appengine/routes/meuperfil/caixaesquerda/itens/itensmeusitens.py
+++ backend/appengine/routes/meuperfil/caixaesquerda/itens/itensmeusitens.py
@@ -22,21 +22,16 @@ __ctx = {'items':'','item':'','erros':'','sucesso':-1,
 def index():
     query = Item.query().order(Item.titulo)
     item_lista = query.fetch()
-
     form = ItemForm()
-
     item_lista = [form.fill_with_model(item) for item in item_lista]
-
     p_editar_form = router.to_path(editar_form)
     p_excluir = router.to_path(excluir)
     for item in item_lista:
         item['path_editar_form'] = '%s/%s'%(p_editar_form,item['id'])
         item['path_excluir'] = '%s/%s'%(p_excluir,item['id'])
-
     __ctx['itens'] = item_lista
     __ctx['erros'] = ''
     __ctx['sucesso'] = -1
-
     return TemplateResponse(__ctx)
 
 @login_required
@@ -46,7 +41,6 @@ def editar(id,**itens):
     item = Item.get_by_id(id)
     item_form = ItemForm(**itens)
     erros = item_form.validate()
-
     __ctx['path_editar'] = router.to_path(editar,id)
     if erros:
         __ctx['erros'] = erros
@@ -57,9 +51,7 @@ def editar(id,**itens):
         item_form.fill_model(item)
         item.put()
         __ctx['sucesso'] = 1
-
         return RedirectResponse(router.to_path(index))
-
 
 @login_required
 @no_csrf
@@ -72,28 +64,12 @@ def editar_form(id):
     __ctx['path_editar'] = router.to_path(editar,id)
     return TemplateResponse(__ctx,'/meuperfil/caixaesquerda/itens/editar_form.html')
 
-'''
-    item_form = ItemForm(**itens)
-    erros = item_form.validate()
-    __ctx['salvar'] = router.to_path(salvar)
-    if erros:
-        __ctx['erros'] = erros
-        __ctx['items'] = item_form
-    else:
-        item = item_form.fill_model()
-        item.put()
-        __ctx['sucesso'] = 1
-    return TemplateResponse(__ctx,'/meuperfil/caixaesquerda/cadastraritens/cadastraritens.html')
-'''
-
-
 @login_required
 @no_csrf
-def excluir(_resp,**itens):
-
-    _resp.write('excluir , '+str(itens))
-    return TemplateResponse(__ctx,'/meuperfil/caixaesquerda/itens/itensmeusitens.html')
-
+def excluir(_resp,id):
+    chave = ndb.Key(Item,int(id))
+    chave.delete()
+    return RedirectResponse(router.to_path(index))
 
 
 
