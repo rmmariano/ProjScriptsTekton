5eef899434f213e9a0babfd3955615a65fb766ca
Rodrigo Mariano <rodmariano13@gmail.com>
2015-05-22 22:59:17 -0300

    refs#7-angular video ate 22.14

--- backend/appengine/routes/meuperfil/caixaesquerda/itens/itensmeusitens.py
+++ backend/appengine/routes/meuperfil/caixaesquerda/itens/itensmeusitens.py
@@ -8,6 +8,8 @@ from tekton.gae.middleware.redirect import RedirectResponse
 from model.db import *
 from tekton.gae.middleware.json_middleware import JsonUnsecureResponse
 from distutils import log
+from json import dumps
+
 
 __author__ = 'Rodrigo'
 
@@ -18,6 +20,55 @@ __ctx = {'items':'','item':'','categorias':'','categoria_selecionada':'','erros'
 
 __dct = {'error':''}
 
+@login_required
+@no_csrf
+def index(id_categoria = None):
+    if id_categoria == None or id_categoria == 'all':
+        query = Item.query().order(Item.titulo)
+        item_lista = query.fetch()
+        if id_categoria == 'all':
+            __ctx['categoria_selecionada'] = 'all'
+        else:
+            __ctx['categoria_selecionada'] = ''
+    else:
+        query = Item.query(Item.id_categoria == id_categoria).order(Item.titulo)
+        item_lista = query.fetch()
+        __ctx['categoria_selecionada'] = id_categoria
+
+    query = Categoria.query().order(Categoria.categoria)
+    categorias = query.fetch()
+
+    if len(item_lista) > 0:
+        form = ItemForm()
+        item_lista = [form.fill_with_model(item) for item in item_lista] #transforma a classe em dicionário para poder adicionar valores dinamicamente
+        p_editar_form = router.to_path(editar_form)
+        p_excluir = router.to_path(excluir)
+        for item in item_lista:
+            item['path_editar_form'] = '%s/%s'%(p_editar_form,item['id'])
+            #item['path_excluir'] = '%s/%s'%(p_excluir,item['id'])
+            item['id_categoria'] = ndb.Key(Categoria,int(item['id_categoria']))
+            for cat in categorias:
+                if item['id_categoria'] == cat.key:
+                    item['categoria'] = cat.categoria
+                    item['id_categoria'] = ''
+                    break
+        __ctx['itens'] = item_lista
+        __ctx['encontrado'] = 1
+    else:
+        __ctx['encontrado'] = 0
+    __ctx['categorias'] = categorias
+    __ctx['erros'] = ''
+    __ctx['sucesso'] = -1
+    __ctx['path_index'] = router.to_path(index)
+    __ctx['path_salvar'] = router.to_path(salvar)
+
+    str_json = dumps(item_lista)
+
+    __ctx['itens'] = str_json
+
+    return TemplateResponse(__ctx)
+
+'''
 @login_required
 @no_csrf
 def index(id_categoria = None, buscar = '0'):
@@ -60,6 +111,7 @@ def index(id_categoria = None, buscar = '0'):
     __ctx['path_salvar'] = router.to_path(salvar)
 
     return TemplateResponse(__ctx)
+'''
 
 @login_required
 @no_csrf
